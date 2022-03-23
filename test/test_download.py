567415ed1eb7a02678bdb80bcc2a4233b98ee741
Harry Whiskard <harry.whiskard@mottmac.com>
2022-03-23 08:49:48 +0000

    add id test

--- test/test_download.py
+++ test/test_download.py
@@ -1,4 +1,17 @@
 import sys
+import pytest
+
 sys.path.append('..')
 
-from src import download_files
+
+from src.download_files import ThingDownloader 
+
+def test_api_connection():
+
+    test_thing = ThingDownloader()
+    
+    user_details = test_thing.get_user_details()
+
+    assert 'id' in user_details, "API connection failed"
+
+test_api_connection()
