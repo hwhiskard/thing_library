99d91f89f6cf92039631d712da0089b7e17ae902
Harry Whiskard <harry.whiskard@mottmac.com>
2022-03-28 17:41:55 +0100

    make object, add rotation and save

--- src/visualise_thing.py
+++ src/visualise_thing.py
@@ -1,22 +1,51 @@
+import math
 from stl import mesh
 from mpl_toolkits import mplot3d
 from matplotlib import pyplot
 
-if __name__=='__main__':
+class ThingViewer():
+    """
+    """
+    def __init__(self, file_path):
+        # Load the STL file
+        self.mesh = mesh.Mesh.from_file(file_path)
 
-    # Create a new plot
-    figure = pyplot.figure()
-    axes = mplot3d.Axes3D(figure)
+    def rotate_mesh(self, x, y, z):
+
+        self.mesh.rotate([0.5, 0, 0], math.radians(x))
+        self.mesh.rotate([0, 0.5, 0], math.radians(y))
+        self.mesh.rotate([0, 0, 0.5], math.radians(z))
+
+    def translate_centre_of_mass(self):
+        pass
+    
+    def visualise_stl(self):
+        """
+        """
+
+        # Create a new plot
+        figure = pyplot.figure()
+        axes = mplot3d.Axes3D(figure)
+        
+
+        axes.add_collection3d(mplot3d.art3d.Poly3DCollection(self.mesh.vectors))
+
+        # Auto scale to the mesh size
+        scale = self.mesh.points.flatten()
+        axes.auto_scale_xyz(scale, scale, scale)
+
+        # Show the plot to the screen
+        pyplot.show()
+
+    def save(self):
+        self.mesh.save(file_path)
+    
+
+if __name__=='__main__':
 
     file_path = r'C:\Users\WHI93526\OneDrive - Mott MacDonald\Documents\thing_files\BT_smooth.stl'
 
-    # Load the STL files and add the vectors to the plot
-    your_mesh = mesh.Mesh.from_file(file_path)
-    axes.add_collection3d(mplot3d.art3d.Poly3DCollection(your_mesh.vectors))
-
-    # Auto scale to the mesh size
-    scale = your_mesh.points.flatten()
-    axes.auto_scale_xyz(scale, scale, scale)
-
-    # Show the plot to the screen
-    pyplot.show()
+    dog = ThingViewer(file_path)
+    mesh = dog.mesh
+    dog.rotate_mesh(0, 0, 90)
+    dog.visualise_stl()
